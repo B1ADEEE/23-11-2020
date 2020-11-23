74794513c9ca8adf549db3d1b87e8bce27f6e7de
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-11-23 11:21:17 +0000

    up to question 5

---
+++ 23-11-2020/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _23_11_2020
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+        public Player(int id,string playername,int score)
+        {
+            ID = id;
+            PlayerName = playername;
+            Score = score;
+        }
+    }
+}
