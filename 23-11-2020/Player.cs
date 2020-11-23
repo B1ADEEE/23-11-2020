f4ab6ac26ec4fa5ee541b5b58a032319ae48372a
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-11-23 11:36:45 +0000

    up to question 7

--- 23-11-2020/Player.cs
+++ 23-11-2020/Player.cs
@@ -8,12 +8,19 @@ namespace _23_11_2020
     {
         public int ID { get; set; }
         public string PlayerName { get; set; }
-        public int Score { get; set; }
+        public int Score { get;private set; }
         public Player(int id,string playername,int score)
         {
             ID = id;
             PlayerName = playername;
             Score = score;
+        }
+        public void IncreaseScore(int ValueToAdd)
+        {
+            if (Score < 100)
+            {
+                Score += ValueToAdd;
+            }
         }
     }
 }
--- 23-11-2020/Program.cs
+++ 23-11-2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _23_11_2020
 {
@@ -11,7 +12,15 @@ namespace _23_11_2020
             Player player3 = new Player(3, "Kevin", 0);
             Player player4 = new Player(4, "Conor", 0);
             Player player5 = new Player(5, "Pauline", 0);
-
+            
+            List <Player> allPlayers = new List<Player>();
+            allPlayers.Add(player1);
+            allPlayers.Add(player2);
+            allPlayers.Add(player3);
+            allPlayers.Add(player4);
+            allPlayers.Add(player5);
+            player1.IncreaseScore(1);
+            player2.IncreaseScore(1);
         }
     }
 }
