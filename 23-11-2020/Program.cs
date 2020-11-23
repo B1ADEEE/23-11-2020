eb6db9b588c6f1a96ae6aead29a74209f1285610
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-11-23 12:15:25 +0000

    question 9

--- 23-11-2020/Program.cs
+++ 23-11-2020/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _23_11_2020
 {
@@ -12,17 +15,32 @@ namespace _23_11_2020
             Player player3 = new Player(3, "Kevin", 0);
             Player player4 = new Player(4, "Conor", 0);
             Player player5 = new Player(5, "Pauline", 0);
-            
-            List <Player> allPlayers = new List<Player>();
+
+            List<Player> allPlayers = new List<Player>();
             allPlayers.Add(player1);
             allPlayers.Add(player2);
             allPlayers.Add(player3);
             allPlayers.Add(player4);
             allPlayers.Add(player5);
-            player1.IncreaseScore(1);
-            player2.IncreaseScore(1);
+            
             Display(allPlayers);
+            GetScores(allPlayers);
         }
+
+        private static void GetScores(List<Player> allPlayers)
+        {
+            Console.WriteLine("\nPlease enter number of players you wish to add score for!");
+            int PlayerNumber = Convert.ToInt32(Console.ReadLine());
+            while (PlayerNumber != 0)
+            {
+                Player selectedPlayer = allPlayers.ElementAt(PlayerNumber - 1);
+                selectedPlayer.IncreaseScore(1);
+                Display(allPlayers);
+                Console.WriteLine("\nPlease enter number of players you wish to add score for!");
+                PlayerNumber = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
         private static void Display (List<Player> players)
         {
             Console.WriteLine("{0,-10}{1,-10}{2,-10}{3,-10}{4,-10}", "Player 1", "Player 2", "Player 3", "Player 4", "Player 5");
